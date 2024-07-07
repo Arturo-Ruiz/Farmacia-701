5fc71307454657ddae014fb5db95caf30852209d
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2024-07-07 07:43:22 -0400

    chore: Update LoginController to use LoginRequest for authentication

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Http\Requests\Auth\LoginRequest;
+
 class LoginController extends Controller{
     public function showLoginForm(){
         return view('admin.auth.login');
     }
 
-    public function login(Request $request){
+    public function login(LoginRequest $request){
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials, $request->remember)) {
             $request->session()->regenerate();
