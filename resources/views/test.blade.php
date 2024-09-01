5ff5b834eefb9106355d66958a0cc754aed78536
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2024-09-01 12:44:53 -0400

    Improve search functionality and layout in test view

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -16,26 +16,25 @@
     <header class="navbar navbar-expand-lg bg-light w-100 shadow py-3">
         <div class="container">
             <div class="row">
+
                 <div class="col-2">
                     <a class="navbar-brand" href="#">
                         <img src="https://www.farmacia701.com/img/logo.png" alt="Logo" width="50" height="50">
                     </a>
                 </div>
 
-                <div class="col-10 row">
-                    <div class="col-10">
-                        <div class="input-group m-2">
-                            <input style="border-radius: 25px 0 0 0;" type="text" class="form-control" placeholder="Busca tu producto aquí" aria-label="Username" aria-describedby="basic-addon1">
-                            <button style="border-radius: 0 0 25px 0;" class="input-group-text">
-                                <i class="bi bi-search"></i>
-                            </button>
-                        </div>
+                <div class="col-8">
+                    <div class="input-group m-2">
+                        <input style="border-radius: 25px 0 0 0;" type="text" class="form-control" placeholder="Busca tu producto aquí" aria-label="Username" aria-describedby="basic-addon1">
+                        <button style="border-radius: 0 0 25px 0;" class="input-group-text">
+                            <i class="bi bi-search"></i>
+                        </button>
                     </div>
-                    <div class="col-2 d-flex justify-content-center align-items-center">
-                        <a class="nav-link" href="#">
-                            <i class="bi bi-cart" style="font-size: 1.5rem;"></i>
-                        </a>
-                    </div>
+                </div>
+                <div class="col-2 d-flex justify-content-center align-items-center">
+                    <a class="nav-link" href="#">
+                        <i class="bi bi-cart" style="font-size: 1.5rem;"></i>
+                    </a>
                 </div>
 
 
