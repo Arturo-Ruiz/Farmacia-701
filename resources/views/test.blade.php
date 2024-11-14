1b51a952e45d524886fed64826f585ba6cd972db
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2024-11-14 17:31:40 -0400

    Upload index

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,60 +1,245 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test View</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="es">
+
+    <title>Farmacia 701 - ¡Somos tus Aliados en Salud!</title>
+
+    <meta name="description" content="Farmacia 701 - ¡Somos tus Aliados en Salud!. Ubicada en Ciudad Bolívar con servicio de entrega a domicilio. Encuentra medicamentos, productos de salud, cuidado personal y suplementos.">
+
+    <meta name="keywords" content="Farmacia, Medicina, Médico, Medicamentos, Salud, Remedio, Farmacia en Ciudad Bolívar, medicamentos Ciudad Bolívar, farmacia cerca de mí, servicio de farmacia a domicilio, entrega de medicamentos Ciudad Bolívar, venta de medicamentos, medicinas Ciudad Bolívar, consultas farmacéuticas, asesoría farmacéutica, vitaminas, productos de cuidado personal, productos de salud, farmacias abiertas Ciudad Bolívar, remedios Ciudad Bolívar, productos naturales, salud Ciudad Bolívar, higiene personal, productos de bebé, suplementos alimenticios">
+
+    <meta name="author" content="Arturo Ruiz">
+
+    <meta name="robots" content="index, follow">
+
+    <link rel="canonical" href="https://farmacia701.com">
+
+    <meta property="og:title" content="Farmacia en Ciudad Bolívar - Medicamentos y Entrega a Domicilio">
+    <meta property="og:description" content="Farmacia en Ciudad Bolívar con servicio de entrega a domicilio. Encuentra medicamentos y productos de salud.">
+    <meta property="og:image" content="https://tu-farmacia.com/imagen.jpg">
+    <meta property="og:url" content="https://tu-farmacia.com">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Farmacia en Ciudad Bolívar">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Farmacia en Ciudad Bolívar - Medicamentos y Entrega a Domicilio">
+    <meta name="twitter:description" content="Farmacia en Ciudad Bolívar con servicio de entrega a domicilio. Encuentra medicamentos y productos de salud.">
+    <meta name="twitter:image" content="https://tu-farmacia.com/imagen-twitter.jpg">
+
+
+    <meta name="geo.region" content="VE-B">
+    <meta name="geo.placename" content="Ciudad Bolívar">
+    <meta name="geo.position" content="8.1292;-63.5400">
+    <meta name="ICBM" content="8.1292, -63.5400">
+
+    <!-- <meta http-equiv="Content-Security-Policy" content="default-src 'self';"> -->
+
+    <meta name="DC.title" content="Farmacia en Ciudad Bolívar">
+    <meta name="DC.creator" content="Farmacia Ciudad Bolívar">
+    <meta name="DC.subject" content="medicamentos, salud, farmacia en Ciudad Bolívar">
+    <meta name="DC.description" content="Farmacia en Ciudad Bolívar con servicio de entrega a domicilio para medicamentos y productos de salud.">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ asset('assets/OwlCarousel/src/owl.carousel.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/OwlCarousel/src/owl.theme.default.min.css') }}" />
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
 </head>
 
-<body>
-    <header class="navbar navbar-expand-lg bg-light w-100 shadow py-3">
-        <div class="container">
-            <div class="row">
+<body class="anuncio delivery">
 
-                <div class="col-2">
-                    <a class="navbar-brand" href="#">
-                        <img src="https://www.farmacia701.com/img/logo.png" alt="Logo" width="50" height="50">
-                    </a>
-                </div>
+    <!-- Navbar -->
+    <header class="navbar navbar-expand navbar-light bg-light shadow py-3">
+        <nav class="container">
 
-                <div class="col-8">
-                    <div class="input-group m-2">
-                        <input style="border-radius: 25px 0 0 0;" type="text" class="form-control" placeholder="Busca tu producto aquí" aria-label="Username" aria-describedby="basic-addon1">
-                        <button style="border-radius: 0 0 25px 0;" class="input-group-text">
-                            <i class="bi bi-search"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="col-2 d-flex justify-content-center align-items-center">
-                    <a class="nav-link" href="#">
-                        <i class="bi bi-cart" style="font-size: 1.5rem;"></i>
-                    </a>
-                </div>
-
-
-
-                <!-- <div class="col-1">
-                    <div class="d-flex flex-column align-items-center" style="min-width: 60px;">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Flag_of_Venezuela.svg/1200px-Flag_of_Venezuela.svg.png" alt="Venezuela Flag" width="30" height="20" class="mb-1">
-                        <span class="fw-bold text-center" style="font-size: 0.875rem;">7.50 Bs/USD</span>
-                    </div>
-                </div> -->
+            <div class="col-3 d-flex align-items-center justify-content-center">
+                <a class="navbar-brand" href="https://www.farmacia701.com/Inicio">
+                    <img src="https://www.farmacia701.com/img/logo.png" alt="" width="50" height="50" />
+                </a>
             </div>
-        </div>
+
+            <form class="d-flex align-items-center col-6 justify-content-center">
+                <input id="input_search" class="form-control form-control-dark input__search " type="text"
+                    placeholder="Buscar" aria-label="Search" />
+                <button type="submit" id="search" class="btn button__blue button__search search">
+                    <i class="fa-solid fa-search"></i>
+                </button>
+                </div>
+            </form>
+
+            <div class="navbar-nav cart__icon col-3 d-flex align-items-center justify-content-center">
+                <a href="https://www.farmacia701.com/Carrito" type="button" class="position-relative button__cart">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-circle bg-secondary">1</span>
+                </a>
+            </div>
+        </nav>
     </header>
 
+    <!-- Search -->
+    <section class="anuncio_collapse">
+        <div class="sidebar">
+            <p class="text-center h6 text-primary text-uppercase fw-bold">Prueba nuestro buscador</p>
+            <p class=" mx-4 text-secondary">Haz tu búsqueda por alfabeto, nombre o componente activo</p>
+        </div>
+    </section>
+
+    <!-- Carousel -->
+
+    <section class="container py-3">
+        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <!--data-bs-interval="2000"-->
+                    <img src="https://www.farmacia701.com/carousel/Carrusel%201.png" class="d-flex w-100 img-fluid"
+                        alt="imagen de carrusel numero uno">
+                </div>
+                <div class="carousel-item active">
+                    <!--data-bs-interval="2000"-->
+                    <img src="https://www.farmacia701.com/carousel/Carrusel%201.png" class="d-flex w-100 img-fluid"
+                        alt="imagen de carrusel numero uno">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+
+    <!-- Zoom  -->
+
+    <section class="separador mt-5 container p-2 ml-2 mr-2">
+        <div class="navbar">
+            <div class="container d-flex justify-content-center align-self-center">
+                <div class="col-12 col-md-2 d-flex justify-content-center justify-content-md-end">
+                    <span class="h6 text-uppercase mt-2 fw-bold text-d">unidos</span>
+                </div>
+                <div class="col-12 col-md-3">
+                    <span class="h6 text-uppercase d-flex justify-content-center align-items-center"> <img src="https://www.farmacia701.com/img/logo.png" alt=""> <i
+                            class="fa-solid fa-plus"></i> <img src="https://www.farmacia701.com/img/zoom.png" alt=""></span>
+                </div>
+
+                <div class="col-12 col-md-4 d-flex justify-content-center justify-content-md-start">
+                    <span class=" h6 text-uppercase mt-2 fw-bold text-d"> Delivery a nivel nacional <i
+                            class="fa-solid fa-truck-fast fs-4"></i></span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Ads -->
+
+    <section id="ads" class="container mt-5">
+        <div class="py-4">
+            <p class="h3 head__title">Lo recomendado</p>
+        </div>
+        <div class="owl-carousel carousel__ads">
+            <div class="test"><img src="https://www.farmacia701.com/ads/Cooper%20Pharma%20Publicidad.png" alt=""></div>
+            <div class="test"><img src="https://www.farmacia701.com/ads/Cooper%20Pharma%20Publicidad.png" alt=""></div>
+            <div class="test"><img src="https://www.farmacia701.com/ads/Cooper%20Pharma%20Publicidad.png" alt=""></div>
+            <div class="test"><img src="https://www.farmacia701.com/ads/Cooper%20Pharma%20Publicidad.png" alt=""></div>
+            <div class="test"><img src="https://www.farmacia701.com/ads/Cooper%20Pharma%20Publicidad.png" alt=""></div>
+        </div>
+    </section>
+
+    <!-- Products -->
+
+    <section id="products" class="container mt-2 mb-5">
+        <div class="py-4">
+            <p class="h3 head__title text-center">Productos</p>
+
+        </div>
+
+
+
+        <!-- Line -->
+        <div id="prueba" class="row justify-content-center product__container">
+
+
+        </div>
+
+
+        <!-- boton cargar mas -->
+        <div class="d-flex justify-content-center mt-3">
+            <button type="button" id="loading" class="btn btn-outline-primary col-4">Cargar más <i
+                    class="fa-solid fa-arrow-down"></i></button>
+        </div>
+
+    </section>
 
 
 
 
+
+    <footer class="bg-primary text-light pt-5 pb-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mb-4">
+                    <div class="rounded-circle bg-white shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
+                        <img src="https://www.farmacia701.com/img/logo.png" height="140px" alt="" />
+                    </div>
+                    <p class="text-center">Encuentra todos tús medicamentos, productos de salud, cuidado personal y suplementos deportivos</p>
+                    <p class="text-center">Úbicanos en Ciudad Bolívar - Venezuela.</p>
+                    <p class="text-center">¡Somos tus Aliados en Salud! <br> Farmacia 701, C.A</p>
+
+
+                </div>
+
+
+                <!-- Mapa de ubicación -->
+                <div class="col-md-6 mb-4">
+                    <h5 class="text-uppercase">Nuestra ubicación</h5>
+                    <div class="map-container" style="height: 250px; border-radius: 8px; overflow: hidden;">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d246.8645807566945!2d-63.54489201416361!3d8.118450401078919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8dce87a89edf52dd%3A0xad68288781bd60fb!2sFarmacia%20701%2C%20C.A.!5e0!3m2!1sen!2sus!4v1731616867323!5m2!1sen!2sus" width="640" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <hr class="border-light">
+
+            <div class="text-center pt-3">
+                <p class="mb-0">&copy; 2024 Farmacia 701. Todos los derechos reservados.</p>
+            </div>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="{{ asset('assets/OwlCarousel/owl.carousel.min.js') }}"></script>
+
+    <script>
+        let anuncio = document.querySelector("body");
+
+        anuncio.addEventListener("click", function() {
+            document.querySelector("body").classList.remove("anuncio");
+        })
+
+        $(".owl-carousel").owlCarousel({
+            items: 4,
+            loop: true,
+            margin: 10,
+            autoplay: true,
+            autoplayTimeout: 1000,
+            autoplayHoverPause: true
+        });
+    </script>
 </body>
 
 </html>
