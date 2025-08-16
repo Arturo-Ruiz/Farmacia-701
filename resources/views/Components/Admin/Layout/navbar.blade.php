9189912194c2a14759db4bcb09aba39e928c7c55
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2025-08-16 17:46:21 -0400

    Add navbar and sidebar components for admin layout; implement user dropdown and dashboard links

---
+++ resources/views/Components/Admin/Layout/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="false">
+    <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+            <h6 class="font-weight-bolder text-white mb-0">@yield('title', 'Página')</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                {{-- Puedes poner un buscador aquí si quieres --}}
+            </div>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item dropdown pe-2 d-flex align-items-center">
+                    <a href="#" class="nav-link text-white font-weight-bold px-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa fa-user me-sm-1"></i>
+                        <span class="d-sm-inline d-none">{{ Auth::user()->name }}</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="dropdown-item border-radius-md">Cerrar Sesión</button>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
