d525468f9b7030ee31b45fd4fbcedde89278a7fc
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2025-08-16 17:42:00 -0400

    Refactor code structure for improved readability and maintainability

--- resources/views/Layouts/guest.blade.php
+++ resources/views/Layouts/guest.blade.php
@@ -9,6 +9,13 @@
 
     <title>@yield('title') | Farmacia 701</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href=" {{ asset('/apple-touch-icon.png') }} " />
+    <meta name="apple-mobile-web-app-title" content="Farmacia 701" />
+    <link rel="manifest" href="{{ asset('/site.webmanifest') }}" />
+
     <meta name="robots" content="noindex, nofollow">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
