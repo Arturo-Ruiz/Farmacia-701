bfeb7b4555f462d3b536ea47313c7d5fc17624f1
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2025-12-30 15:50:12 -0400

    feat: add 503 error page for SSL certificate issues

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>503 Update SSL certificate</title>
+</head>
+<body>
+  <h1>503 Update SSL certificate</h1>
+  <p>The request cannot be completed because the server's SSL certificate has expired or is being replaced. Ensure that the new certificate has been installed and properly configured on the load balancer or reverse proxy.
+    The server was unable to complete your request. Please try again later.</p>
+</body>
+</html>
